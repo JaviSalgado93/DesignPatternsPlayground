4430a01fcdc0851f2aa674523ff445037c281273
Javier Salgado <javi-salgado93@hotmail.com>
2026-01-14 18:18:35 -0500

    feat: agregar componentes UI compartidos (Breadcrumb, Badge, Accordion, Alert, Tabs, Section)

---
+++ src/DesignPatterns.Web/Components/Common/BreadcrumbItem.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Web.Components.Common;
+
+public class BreadcrumbItem
+{
+    public string Label { get; set; } = "";
+    public string Url { get; set; } = "";
+
+    public BreadcrumbItem() { }
+
+    public BreadcrumbItem(string label, string url = "")
+    {
+        Label = label;
+        Url = url;
+    }
+}
